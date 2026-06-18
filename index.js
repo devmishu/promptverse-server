4a1a83593e0b5be769931332a9d3e2c15298728f
Mishu <mishudebnath927@gmail.com>
2026-06-19 01:31:11 +0600

    add start script

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function run() {
 
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        await client.connect();
 
         // prompts related api 
         app.post('/api/prompts', async (req, res) => {
